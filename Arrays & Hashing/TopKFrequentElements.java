7c84c8cdabeff47f47dfaad50a59959bef05fc4f
sahil-sharma-cs <sahilsharma_081995@yahoo.com>
2025-08-24 13:31:08 +0530

    Update TopKFrequentElements.java

--- Arrays & Hashing/TopKFrequentElements.java
+++ Arrays & Hashing/TopKFrequentElements.java
@@ -1,41 +1,7 @@
 # https://neetcode.io/problems/top-k-elements-in-list?list=blind75
 
-/* solution 1: basically put frequency of numbers in a map and the iterate that map target (k) times so that you have 
-added k most occuring numbers in the resulting array
-*/  
-public static int[] topKFrequent(int[] nums, int target) {
 
-    Map<Integer, Integer> map = new HashMap<>();
-    for(int i=0; i<nums.length; i++) {
-  
-        if(map.containsKey(nums[i])) {
-            map.put(nums[i], map.get(nums[i]) + 1);
-        } else {
-            map.put(nums[i], 1);
-        }
-    }
-  
-    ArrayList<Integer> rArr = new ArrayList();
-  
-  
-    while(target>0) {
-        int maxIndex = -1;
-        int maxNum = -1;
-        for(Map.Entry<Integer, Integer> m : map.entrySet()) {
-            if(m.getValue()>maxNum) {
-                maxNum = Math.max(m.getValue(), maxNum);
-                maxIndex = m.getKey();
-            }
-  
-        }
-        rArr.add(maxIndex);
-        map.remove(maxIndex);
-        target--;
-    }
-    return rArr.stream().mapToInt(Integer::intValue).toArray();
-}
-
-#solution 2: use bucket sort
+#solution 1: use bucket sort
 /*
 1. again count the frequency of number and put it in a map
 2. fill a resulting array with -1 first and then put the frequency number in index of array, 
@@ -94,4 +60,44 @@ class Solution {
 
         return rArr.stream().mapToInt(i->i).toArray();
     }
+
+    public static void main(String[] args) {
+        System.out.println(topKFrequent(new int[]{1,2,2,3,3,3}, 2));
+    }
+
+    /* solution 2: basically put frequency of numbers in a map and the iterate that map target (k) times so that you have 
+    added k most occuring numbers in the resulting array
+    */  
+    public static int[] topKFrequent(int[] nums, int target) {
+    
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+      
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+      
+        ArrayList<Integer> rArr = new ArrayList();
+      
+      
+        while(target>0) {
+            int maxIndex = -1;
+            int maxNum = -1;
+            for(Map.Entry<Integer, Integer> m : map.entrySet()) {
+                if(m.getValue()>maxNum) {
+                    maxNum = Math.max(m.getValue(), maxNum);
+                    maxIndex = m.getKey();
+                }
+      
+            }
+            rArr.add(maxIndex);
+            map.remove(maxIndex);
+            target--;
+        }
+        return rArr.stream().mapToInt(Integer::intValue).toArray();
+    }
+
 }
